a2cb4878c911473a6dc95f586ef23945501f6e95
Fabiola Paz <fabiihpaaz@gmail.com>
2022-08-25 21:31:50 -0300

    Calculadora

---
+++ index.js
@@ -0,0 +1,17 @@
+function minhaFuncao (){
+   console.log("Resultado da soma: ")
+}
+
+function somando (a,b){
+   return a+b;
+}
+
+const somar = (a,b) => {return a+b}
+
+minhaFuncao();
+
+let total = somando(5,8);
+let resultado = somar(10,4);
+
+console.log(total)
+console.log(resultado)
